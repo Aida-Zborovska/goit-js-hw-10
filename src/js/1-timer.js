da20b368abbe80972f488650cb47be735d201cb3
Aїda <izograf.zinaida@gmail.com>
2025-11-14 02:00:16 +0200

    done timer task except notifications

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,3 +2,88 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button');
+const timer = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    calendarCloseHandler(selectedDates[0]);
+  },
+};
+
+flatpickr(input, options);
+
+startBtn.disabled = true;
+startBtn.addEventListener('click', timerStartHandler);
+
+function calendarCloseHandler(selectedDate) {
+  const nowDate = new Date();
+  if (selectedDate <= nowDate) {
+    startBtn.disabled = true;
+    window.alert('Please choose a date in the future');
+    return;
+  }
+  startBtn.disabled = false;
+  userSelectedDate = selectedDate;
+}
+
+function timerStartHandler() {
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  startCountdown(userSelectedDate);
+}
+
+function startCountdown(finishDate) {
+  const intervalId = setInterval(() => {
+    const nowDate = new Date();
+    const diff = finishDate - nowDate;
+    if (diff < 0) {
+      timerFinishHandler(intervalId);
+      return;
+    }
+    updateTimerDisplay(convertMs(diff));
+  }, 1000);
+}
+
+function timerFinishHandler(intervalId) {
+  clearInterval(intervalId);
+  input.disabled = false;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  timer.days.textContent = addLeadingZero(days);
+  timer.hours.textContent = addLeadingZero(hours);
+  timer.minutes.textContent = addLeadingZero(minutes);
+  timer.seconds.textContent = addLeadingZero(seconds);
+}
